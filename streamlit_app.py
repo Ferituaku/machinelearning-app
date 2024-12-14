935ea8f0580dbe4defbb4358bc9e6564eedb5aea
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-12-14 21:24:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Project Aplikasi Model Prediksi')
 
 st.write('Hai, Selamat Datang!')
+
+with st.expander('Data'):
+  st.write('**Raw Data')
+  df = pd.read_csv("https://raw.githubusercontent.com/Ferituaku/machinelearning-app/refs/heads/master/data.csv")
+  df
